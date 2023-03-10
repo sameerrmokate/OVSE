ddcde85e10d13ae262e251390b0ddd47f8a0da6d
Mishra-shashank04 <mishrashashankshekhar505@gmail.com>
2023-03-10 20:26:28 +0530

    some changes

--- LogIn&RegisterPage/Frontend/src/App.js
+++ LogIn&RegisterPage/Frontend/src/App.js
@@ -1,13 +1,17 @@
 import Login from "./Components/login";
 import Register from "./Components/Register";
-import Home from "./Components/Home"
-import {createBrowserRouter,RouterProvider} from "react-router-dom"
+import Home from "./Components/Home";
+import Voting_tokens from "./Components/Voting_tokens"
+import Nomination from "./Components/Nomination"
+import {createBrowserRouter,Form,RouterProvider} from "react-router-dom"
 // import "./style.css"
 
 const router = createBrowserRouter([
   {path : "/home" , element: <Home/>},
   {path : '/', element: <Login/>},
-  {path : "/register" , element : <Register/>}
+  {path : "/register" , element : <Register/>},
+  {path : "/Voting_tokens" , element: <Voting_tokens/>},
+  {path : "/Nomination" , element: <Nomination/>}
 ])
 
 function App() {
